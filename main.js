616caa1057bdab47a8c502d1e2f1fba1742e34ea
Oliver932 <76821440+Oliver932@users.noreply.github.com>
2021-04-01 09:46:36 +0100

    cell object

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var cellSize = 3;
 var columns = Math.floor(innerWidth / cellSize);
 var rows = Math.floor(innerHeight / cellSize);
 
+var perlinScale = 0.04
+var perlinThreshold = 0.5;
 
 function setup() {
 
     createCanvas(columns * cellSize, rows * cellSize);
-
+    createCells();
 
 }
 
@@ -18,9 +20,36 @@ function draw() {
 
     drawGrid();
 
+    for (let index = 0; index < Cells.length; index++) {
+        const cell = Cells[index];
+
+        cell.draw();
+        
+    }
+
 }
 
 
+var Cells = [];
+
+class Cell {
+
+    constructor(x, y) {
+
+        this.x = x;
+        this.y = y;
+
+        Cells.push(this);
+
+    }
+
+    draw(){
+
+        fill(0);
+        noStroke();
+        rect(this.x * cellSize, this.y * cellSize, cellSize, cellSize)
+    }
+}
 
 function drawGrid(){
 
@@ -38,6 +67,22 @@ function drawGrid(){
     }
 
 
+}
+
+function createCells() {
+
+    for (let x = 0; x < columns; x++) {
+
+        for (let y = 0; y < rows; y++) {
+
+            var val = noise(x * perlinScale, y * perlinScale)
+
+            if (val > perlinThreshold) {
+
+                new Cell(x, y);
+            }
+        }
+    }
 }
 
 
